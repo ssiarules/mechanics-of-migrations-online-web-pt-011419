c897e587ba64242c0dc53d43a0f7b26c0e6ae00d
Johnson Joseph <jhonsonj88@gmail.com>
2019-04-07 16:08:28 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_favorite_food_to_artists.rb
+++ db/migrate/02_add_favorite_food_to_artists.rb
@@ -1,5 +1,5 @@
 class AddFavoriteFoodToArtists < ActiveRecord::Migration
   def change
-    add_column :artists, :favorite_food, :string
-  end
+   add_column :artists, :favorite_food, :string
+  end 
 end
